d6bad2f9fb55362a0f7f868b2c001049cf82d13b
felipe_marin_st <flpmarin@digitechfp.com>
2025-06-09 00:37:56 +0200

    Enhance seeders: improve organization and availability creation logic with better handling of coach sports and time formatting

--- database/seeders/SpecificAvailabilitySeeder.php
+++ database/seeders/SpecificAvailabilitySeeder.php
@@ -18,19 +18,25 @@ class SpecificAvailabilitySeeder extends Seeder
         foreach ($coaches as $coach) {
             $sports = CoachSport::where('coach_id', $coach->id)->pluck('sport_id');
 
+            if ($sports->isEmpty()) {
+                continue; // Saltar si el coach no tiene deportes asignados
+            }
+
             foreach (range(1, 5) as $i) {
                 $date = Carbon::now()->addDays(rand(1, 15));
-                $start = '0' . rand(8, 9) . ':00';
-                $end = '1' . rand(0, 1) . ':00';
+
+                // Horas en bloques válidos (ej: 08:00:00 a 09:00:00)
+                $startTime = Carbon::createFromTime(rand(8, 16), 0, 0)->format('H:i:s');
+                $endTime = Carbon::createFromTime(Carbon::parse($startTime)->hour + 1, 0, 0)->format('H:i:s');
 
                 SpecificAvailability::create([
                     'coach_id' => $coach->id,
                     'sport_id' => $sports->random(),
-                    'date' => $date->format('Y-m-d'),
-                    'start_time' => $start,
-                    'end_time' => $end,
+                    'date' => $date->copy()->startOfDay()->setTimezone('UTC'), // UTC explícito
+                    'start_time' => $startTime,
+                    'end_time' => $endTime,
                     'capacity' => rand(1, 5),
-                    'is_online' => rand(0, 1),
+                    'is_online' => (bool) rand(0, 1),
                     'location' => 'Ubicación ' . Str::random(4),
                     'is_booked' => false,
                 ]);
